fd5a1d07e776cfb8798a849320ed032f280ebc2a
Ana Pastor <anapastor@Anas-MBP.home>
2024-07-11 17:05:01 +0200

    Second commit

---
+++ src/main/java/com/example/neocatecumenalWay/NeocatecumenalWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.neocatecumenalWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeocatecumenalWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeocatecumenalWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neocatecumenalWay/NeocatecumenalWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neocatecumenalWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeocatecumenalWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
